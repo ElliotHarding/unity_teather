a25a936cdd5ac3be2d82251b310e3331cc1c8418
Elliot <elliot.one.harding@gmail.com>
2021-07-02 16:37:00 +0100

    Setup home to load new game

---
+++ HomeUI.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HomeUI : MonoBehaviour
+{
+    public void StartButton()//cant call it start
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
