529da0dd7a7e83ba215638f0867dc78918138f61
Elliot <elliot.one.harding@gmail.com>
2021-07-25 13:46:00 +0100

    Panning and moving

--- LevelCreator/LevelCreator.cs
+++ LevelCreator/LevelCreator.cs
@@ -4,26 +4,48 @@ using UnityEngine;
 
 public class LevelCreator : MonoBehaviour
 {
+    //Dragging
     public GameObject m_selectedGameObject;
     bool m_bIsDragging = false;
+    
     public List<GameObject> m_gameObejcts = new List<GameObject>();
+
+    //Panning
+    public float m_panSpeed = 100;
+    bool m_bIsPanning = false;
+    Vector3 m_oldPanPosition;
 
     // Update is called once per frame
     void Update()
     {
+
         if (Input.GetMouseButton(0))
         {
             Vector3 touchPos = UnityEngine.Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-            if(!m_bIsDragging)
+            if (m_bIsPanning)
+            {
+                Vector3 difference = m_oldPanPosition - touchPos;
+                UnityEngine.Camera.main.transform.position += difference * Time.deltaTime * m_panSpeed;
+
+                m_oldPanPosition = touchPos;
+            }
+            else if (!m_bIsDragging)
             {
                 Collider2D collider = Physics2D.OverlapCircle(touchPos, 1);
                 if (collider)
                 {
                     m_selectedGameObject = collider.gameObject;
                     m_bIsDragging = true;
+                    m_bIsPanning = false;
                     m_selectedGameObject.transform.position = new Vector3(touchPos.x, touchPos.y, 0);
                 }
+                else
+                {
+                    m_bIsPanning = true;
+                    m_bIsDragging = false;
+                    m_oldPanPosition = touchPos;
+                }                
             }
             else
             {
@@ -33,6 +55,7 @@ public class LevelCreator : MonoBehaviour
         else
         {
             m_bIsDragging = false;
+            m_bIsPanning = false;
         }
 
         
